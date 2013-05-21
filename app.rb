cf89d4349467e97da9f6d66e048424e213ee5b31
Aaron Lerch <aaronlerch@gmail.com>
2013-05-21 17:13:36 -0400

    Removing leading spaces from script block

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ get '/echofile' do
   halt 404 if params[:content].nil? or params[:file].nil?
   content_type "text/x-shellscript"
   <<-eos
-    #!/bin/sh
-    echo "#{params[:content]}" > #{params[:file]}
+#!/bin/sh
+echo "#{params[:content]}" > #{params[:file]}
   eos
 end

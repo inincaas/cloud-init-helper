aa5a61dd57d89a078c210f40f7a6ba84ac27dc09
Aaron Lerch <aaronlerch@gmail.com>
2013-05-21 17:11:52 -0400

    Write out directive as first line of script file

--- app.rb
+++ app.rb
@@ -7,5 +7,8 @@ end
 get '/echofile' do
   halt 404 if params[:content].nil? or params[:file].nil?
   content_type "text/x-shellscript"
-  "echo \"#{params[:content]}\" > #{params[:file]}"
+  <<-eos
+    #!/bin/sh
+    echo "#{params[:content]}" > #{params[:file]}
+  eos
 end
